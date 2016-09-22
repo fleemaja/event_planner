283a872b39bbbd21158e0f460d25dadeb18dc8c2
fleemaja <drew.fleeman@gmail.com>
2016-09-22 23:31:50 +0000

    add more fields to events and autofocus all first inputs

--- app/models/events.js
+++ app/models/events.js
@@ -5,7 +5,14 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var EventSchema = new Schema({
-	  title: { type: String, required: true }
+	  title: { type: String, required: true },
+	  eventType: { type: String, required: true },
+	  eventHost: { type: String, required: true },
+	  startDate: { type: String, required: true },
+	  endDate: { type: String, required: true },
+	  guestList: [String],
+	  eventLocation: { type: String, required: true },
+	  message: String
 });
 
 // Automatically remove HTML from public facing fields on save
@@ -16,7 +23,14 @@ EventSchema.pre('save', function(next) {
   };
 
   this.title = sanitizeHtml(this.title, sanitize);
-  this.title = this.title.slice(0, 140);
+  this.eventType = sanitizeHtml(this.eventType, sanitize);
+  this.eventHost = sanitizeHtml(this.eventHost, sanitize);
+  this.guestList = this.guestList.map(function(guest){
+    guest = sanitizeHtml(guest, sanitize);
+    return guest;
+  });
+  this.eventLocation = sanitizeHtml(this.eventLocation, sanitize);
+  this.message = sanitizeHtml(this.message, sanitize);
   next();
 });
 
