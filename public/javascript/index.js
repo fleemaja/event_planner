02fb3492baaacbafbad264e92dd48d8e9631fd39
fleemaja <drew.fleeman@gmail.com>
2016-09-24 17:28:11 +0000

    gulp build process for sass to css, css autoprefixing, and asset minification

---
+++ public/javascript/index.js
@@ -0,0 +1,36 @@
+var Guest = 3;
+$('#add-guest').click(function(e) {
+  $('#event-guestList').append('<input type="text" class="form-control form-guestList" placeholder="Guest ' + Guest + '" >');
+  Guest += 1;
+});
+
+$('#event-guestList').change(function() {
+  var guests = [];
+  $(".form-guestList").each(function() {
+    var guestVal = $(this).val().replace(/\,/g, '&#44;');
+    guests.push(guestVal);
+  });
+  $('#guestList').val(guests);
+});
+		
+var signupModal = $('#signupModal');
+var loginModal = $('#loginModal');
+var newEventModal = $('#newEventModal');
+	
+$('.close').on('click', function() {
+	signupModal.hide();
+	loginModal.hide();
+	newEventModal.hide();
+});
+
+$('.signup').on('click', function(e) {
+  signupModal.show();
+});
+
+$('.login').on('click', function(e) {
+  loginModal.show();
+});
+
+$('#newEvent').on('click', function(e) {
+  newEventModal.show();
+});
