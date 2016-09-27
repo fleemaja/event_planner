ae147ed4d9fff00e9b032146b67d858b80a99dfb
fleemaja <drew.fleeman@gmail.com>
2016-09-27 22:04:56 +0000

    format dates before presenting

--- app/models/events.js
+++ app/models/events.js
@@ -15,6 +15,48 @@ var EventSchema = new Schema({
 	  message: String
 });
 
+var months = {
+  '01': 'Jan',
+  '02': 'Feb',
+  '03': 'Mar',
+  '04': 'Apr',
+  '05': 'May',
+  '06': 'Jun',
+  '07': 'Jul',
+  '08': 'Aug',
+  '09': 'Sep',
+  '10': 'Oct',
+  '11': 'Nov',
+  '12': 'Dec'
+}
+
+function getDay(day) {
+  return parseInt(day, 10);
+}
+
+function getFormattedTime(fourDigitTime) {
+    var hours24 = parseInt(fourDigitTime.substring(0, 2),10);
+    var hours = ((hours24 + 11) % 12) + 1;
+    var amPm = hours24 > 11 ? 'pm' : 'am';
+    var minutes = fourDigitTime.substring(2);
+
+    return hours + ':' + minutes + amPm;
+};
+
+function formatDateTime(dateTime) {
+  // dateTime example string: '2016-10-11T03:00'
+  var dateTimeSplit = dateTime.split("T");
+  var date = dateTimeSplit[0]; var time = dateTimeSplit[1];
+  var dateSplit = date.split("-");
+  var year = dateSplit[0]; var month = months[dateSplit[1]]; 
+  var day = getDay(dateSplit[2]);
+  
+  var timeSplit = time.split(":");
+  var time = getFormattedTime(timeSplit[0] + timeSplit[1]);
+  
+  return month + " " + day + ", " + year + " " + time;
+}
+
 // Automatically remove HTML from public facing fields on save
 EventSchema.pre('save', function(next) {
   var sanitize = {
@@ -32,6 +74,8 @@ EventSchema.pre('save', function(next) {
   this.guestList = this.guestList.filter(function(guest) {
     return guest != "";
   });
+  this.startDate = formatDateTime(this.startDate);
+  this.endDate = formatDateTime(this.endDate);
   this.eventLocation = sanitizeHtml(this.eventLocation, sanitize);
   this.message = sanitizeHtml(this.message, sanitize);
   next();
